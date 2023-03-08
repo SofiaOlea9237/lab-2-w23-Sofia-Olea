edc5465d685e47da6b619d0412d83109393c28fe
SofiaOleaYerena <olea2317@adsb.ca>
2023-03-08 09:44:02 -0500

    Initial commit

---
+++ src/main/java/lab2/PracticeExercise_2_1.java
@@ -0,0 +1,13 @@
+package lab2;
+
+public class PracticeExercise_2_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Mississippi");
+        System.out.println("And now is written incorrectly:");
+        String aString="Mississippi".
+                replace("i","ii").replace("ss","s");
+        System.out.println(aString);
+    }
+}
---
+++ src/main/java/lab2/PracticeExercise_2_6.java
@@ -0,0 +1,12 @@
+package lab2;
+
+import java.awt.*;
+
+public class PracticeExercise_2_6
+{
+    public static void main(String[] args)
+    {
+        Rectangle box = new Rectangle(5,10,20,30);
+        box.add(0,0);
+    }
+}

310560977edf5fe18dfa9d0208ace575b12a9b62
SofiaOleaYerena <olea2317@adsb.ca>
2023-03-10 10:01:48 -0500

    DONE

--- src/main/java/lab2/PracticeExercise_2_1.java
+++ src/main/java/lab2/PracticeExercise_2_1.java
@@ -5,7 +5,7 @@ public class PracticeExercise_2_1
     public static void main(String[] args)
     {
         System.out.println("Mississippi");
-        System.out.println("And now is written incorrectly:");
+        System.out.print("And now is written incorrectly: ");
         String aString="Mississippi".
                 replace("i","ii").replace("ss","s");
         System.out.println(aString);
